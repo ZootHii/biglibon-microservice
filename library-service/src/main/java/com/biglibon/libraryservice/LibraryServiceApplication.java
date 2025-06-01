4f2439701d5ae52e7a3bbecea689ac95fd29f5f5
zoothii <ahmet-yildirim_5858@outlook.com>
2025-06-01 20:02:26 +0300

    dockerize

--- library-service/src/main/java/com/biglibon/libraryservice/LibraryServiceApplication.java
+++ library-service/src/main/java/com/biglibon/libraryservice/LibraryServiceApplication.java
@@ -2,9 +2,11 @@ package com.biglibon.libraryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients
 public class LibraryServiceApplication {
 
